f2024517a38f9c19781da033c2a1a6fd8833f562
Sky <sky@Skys-iMac-Pro.local>
2022-05-18 18:30:41 +0800

    Adding contants, updating main page css, updating main page structure

---
+++ src/constants/index.tsx
@@ -0,0 +1,1 @@
+export const API_PRODUCTS_LINK = "https://fakestoreapi.com/products";
--- src/container/shop-app.tsx
+++ src/container/shop-app.tsx
@@ -1,182 +1,176 @@
 import * as React from "react";
-import lodash from 'lodash';
+import lodash from "lodash";
 import Modal from "react-modal";
 import { FaTimes } from "react-icons/fa";
-import { Button } from "./components/button";
-import ProductList from "./components/product-list-components";
-import { Form } from "./components/form";
+import { Button } from "../components/Button/button";
+import ProductList from "../components/Product-List/product-list-components";
+import { Form } from "../components/Form/form";
 import logo from "./images/droppe-logo.png";
 import img1 from "./images/img1.png";
 import img2 from "./images/img2.png";
 import styles from "./shopApp.module.css";
+import * as Constants from "../constants";
 
-export class ShopApp extends React.Component<
-  {},
-  { products: any[]; isOpen: boolean; isShowingMessage: boolean; message: string; numFavorites: number; prodCount: number }
-> {
-  constructor(props: any) {
-    super(props);
+interface IShopAppProps {
+    products: any[];
+    isOpen: boolean;
+    isShowingMessage: boolean;
+    message: string;
+    numFavorites: number;
+    prodCount: number;
+}
 
-    this.favClick = this.favClick.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+export class ShopApp extends React.Component<{}, IShopAppProps> {
+    constructor(props: any) {
+        super(props);
 
-    this.state = { products: [], isOpen: false, isShowingMessage: false, message: '', numFavorites: 0, prodCount: 0 };
+        this.favClick = this.favClick.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
-    fetch('https://fakestoreapi.com/products').then((response) => {
-      let jsonResponse = response.json();
-
-      jsonResponse.then((rawData) => {
-        let data = [];
-
-        for (let i = 0; i < rawData.length; i++) {
-          let updatedProd = rawData[i];
-          data.push(updatedProd);
-        }
-        this.setState({
-          products: data,
-        });
-        this.setState({
-          prodCount: data.length
-        })
-      });
-    });
-  }
-
-   componentDidMount(){
-      document.title = "Droppe refactor app"
-   }
-
-  favClick(title: string) {
-    const prods = this.state.products;
-    const idx = lodash.findIndex(prods, {title: title})
-    let currentFavs = this.state.numFavorites
-    let totalFavs: any;
-
-    if (prods[idx].isFavorite) {
-      prods[idx].isFavorite = false;
-      totalFavs = --currentFavs
-    } else {
-      totalFavs = ++currentFavs
-      prods[idx].isFavorite = true;
+        this.state = { products: [], isOpen: false, isShowingMessage: false, message: "", numFavorites: 0, prodCount: 0 };
     }
 
-    this.setState(() => ({ products: prods, numFavorites: totalFavs }));
-  }
+    componentDidMount() {
+        fetch(Constants.API_PRODUCTS_LINK).then((response) => {
+            let jsonResponse = response.json();
 
-  onSubmit(payload: { title: string; description: string, price: string }) {
-    const updated = lodash.clone(this.state.products);
-    updated.push({
-      title: payload.title,
-      description: payload.description,
-      price: payload.price
-    });
+            jsonResponse.then((rawData) => {
+                let productsResponse = Array.isArray(rawData) ? rawData : [];
 
-    this.setState({
-      products: updated,
-      prodCount: lodash.size(this.state.products) + 1
-    });
+                this.setState({
+                    products: productsResponse,
+                    prodCount: productsResponse.length,
+                });
+            });
+        });
+    }
 
-    this.setState({
-      isOpen: false,
-    });
+    favClick(title: string) {
+        const prods = this.state.products;
+        const idx = lodash.findIndex(prods, { title: title });
+        let currentFavs = this.state.numFavorites;
+        let totalFavs: any;
 
-    this.setState({
-      isShowingMessage: true,
-      message: 'Adding product...'
-    })
+        if (prods[idx].isFavorite) {
+            prods[idx].isFavorite = false;
+            totalFavs = --currentFavs;
+        } else {
+            totalFavs = ++currentFavs;
+            prods[idx].isFavorite = true;
+        }
 
-    // **this POST request doesn't actually post anything to any database**
-    fetch('https://fakestoreapi.com/products',{
-            method:"POST",
-            body:JSON.stringify(
-                {
-                    title: payload.title,
-                    price: payload.price,
-                    description: payload.description,
-                }
-            )
+        this.setState(() => ({ products: prods, numFavorites: totalFavs }));
+    }
+
+    onSubmit(payload: { title: string; description: string; price: string }) {
+        const updated = lodash.clone(this.state.products);
+        updated.push({
+            title: payload.title,
+            description: payload.description,
+            price: payload.price,
+        });
+
+        this.setState({
+            products: updated,
+            prodCount: lodash.size(this.state.products) + 1,
+        });
+
+        this.setState({
+            isOpen: false,
+        });
+
+        this.setState({
+            isShowingMessage: true,
+            message: "Adding product...",
+        });
+
+        // **this POST request doesn't actually post anything to any database**
+        fetch(Constants.API_PRODUCTS_LINK, {
+            method: "POST",
+            body: JSON.stringify({
+                title: payload.title,
+                price: payload.price,
+                description: payload.description,
+            }),
         })
-            .then(res=>res.json())
-            .then(json => {
-               (function (t) {
-                 setTimeout(()=>{
-                    t.setState({
-                       isShowingMessage: false,
-                       message: ''
-                    })
-                 }, 2000)
-              })(this);
-            })
-  }
+            .then((res) => res.json())
+            .then((json) => {
+                (function (t) {
+                    setTimeout(() => {
+                        t.setState({
+                            isShowingMessage: false,
+                            message: "",
+                        });
+                    }, 2000);
+                })(this);
+            });
+    }
 
-  render() {
-    const { products, isOpen } = this.state;
-    return (
-      <React.Fragment>
-        <div className={styles.header}>
-          <div className={['container', styles.headerImageWrapper].join(' ')}>
-            <img src={logo} className={styles.headerImage} />
-          </div>
-        </div>
+    render() {
+        const { products, isOpen } = this.state;
+        return (
+            <React.Fragment>
+                <div className={styles.header}>
+                    <div className={["container", styles.headerImageWrapper].join(" ")}>
+                        <img src={logo} className={styles.headerImage} />
+                    </div>
+                </div>
 
-        <>
-           <span
-              className={['container', styles.main].join(' ')}
-              style={{margin: '50px inherit', display: 'flex', justifyContent: 'space-evenly'}}
-           >
-            <img src={img1} style={{maxHeight: "15em", display: 'block'}} />
-            <img src={img2} style={{maxHeight: "15rem", display: 'block'}} />
-           </span>
-        </>
+                <>
+                    <span className={["container", styles.main].join(" ")} style={{ margin: "50px inherit", display: "flex", justifyContent: "space-evenly" }}>
+                        <img src={img1} style={{ maxHeight: "15em", display: "block" }} />
+                        <img src={img2} style={{ maxHeight: "15rem", display: "block" }} />
+                    </span>
+                </>
 
-        <div className={['container', styles.main].join(' ')} style={{paddingTop: 0}}>
-          <div className={styles.buttonWrapper}>
-            <span role="button">
-               <Button
-                  onClick={function (this: any) {
-                     this.setState({
-                        isOpen: true,
-                     });
-                  }.bind(this)}
-               >Send product proposal</Button>
-            </span>
-             {this.state.isShowingMessage && <div className={styles.messageContainer}>
-                <i>{this.state.message}</i>
-             </div>}
-          </div>
+                <div className={["container", styles.main].join(" ")} style={{ paddingTop: 0 }}>
+                    <div className={styles.buttonWrapper}>
+                        <span role="button">
+                            <Button
+                                onClick={function (this: any) {
+                                    this.setState({
+                                        isOpen: true,
+                                    });
+                                }.bind(this)}
+                            >
+                                Send product proposal
+                            </Button>
+                        </span>
+                        {this.state.isShowingMessage && (
+                            <div className={styles.messageContainer}>
+                                <i>{this.state.message}</i>
+                            </div>
+                        )}
+                    </div>
 
-          <div className={styles.statsContainer}>
-            <span>Total products: {this.state.prodCount}</span>
-            {' - '}
-            <span>Number of favorites: {this.state.numFavorites}</span>
-          </div>
+                    <div className={styles.statsContainer}>
+                        <span>Total products: {this.state.prodCount}</span>
+                        {" - "}
+                        <span>Number of favorites: {this.state.numFavorites}</span>
+                    </div>
 
-          {products && !!products.length ? <ProductList products={products} onFav={this.favClick} /> : <div></div>}
-        </div>
+                    {products && !!products.length ? <ProductList products={products} onFav={this.favClick} /> : <div></div>}
+                </div>
 
-        <>
-           <Modal
-              isOpen={isOpen}
-              className={styles.reactModalContent}
-              overlayClassName={styles.reactModalOverlay}
-           >
-              <div className={styles.modalContentHelper}>
-                 <div
-                    className={styles.modalClose}
-                    onClick={function (this: any) {
-                       this.setState({
-                          isOpen: false,
-                       });
-                    }.bind(this)}
-                 ><FaTimes /></div>
+                <>
+                    <Modal isOpen={isOpen} className={styles.reactModalContent} overlayClassName={styles.reactModalOverlay}>
+                        <div className={styles.modalContentHelper}>
+                            <div
+                                className={styles.modalClose}
+                                onClick={function (this: any) {
+                                    this.setState({
+                                        isOpen: false,
+                                    });
+                                }.bind(this)}
+                            >
+                                <FaTimes />
+                            </div>
 
-                 <Form
-                    on-submit={this.onSubmit}
-                 />
-              </div>
-           </Modal>
-        </>
-      </React.Fragment>
-    );
-  }
+                            <Form on-submit={this.onSubmit} />
+                        </div>
+                    </Modal>
+                </>
+            </React.Fragment>
+        );
+    }
 }
