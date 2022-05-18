5bfd79a334e81fa3d2c735112fe923f6481d8d29
Sky <sky@Skys-iMac-Pro.local>
2022-05-18 18:34:57 +0800

    Define button Props

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import styles from "./button.module.css";
 
-interface props {
-  children: any;
-  onClick?: () => void;
+interface IButtonProps {
+    children: any;
+    onClick?: () => void;
 }
 
-export const Button: React.FC<props> = ({ children, onClick }) => (
-  <button className={styles.button} onClick={onClick}>
-    {children}
-  </button>
+export const Button: React.FC<IButtonProps> = ({ children, onClick }) => (
+    <button className={styles.button} onClick={onClick}>
+        {children}
+    </button>
 );
